13c347fe8de135caf42600cacf143515d8501437
Marvin Fuchs <info@marvin-fuchs.de>
2024-05-23 19:31:05 +0200

    Fix centering

--- frontend/src/components/landingPage/GameCardContainer.vue
+++ frontend/src/components/landingPage/GameCardContainer.vue
@@ -7,14 +7,26 @@ export default {
     },
 
     mounted() {
-        let cardWidth = 200;
-        let gapWidth = 13;
-        let paddingContainer = 30;
-        let gap = (this.$refs.wrapper.offsetWidth - 2 * paddingContainer) % (cardWidth + gapWidth);
-        if (gap < cardWidth) {
-            this.$data.centeringOffset = gap / 2;
-        } else {
-            this.$data.centeringOffset =  (gap - cardWidth) / 2;
+        this.centerWrapper()
+    },
+
+    created() {
+        window.addEventListener("resize", this.centerWrapper);
+    },
+    destroyed() {
+        window.removeEventListener("resize", this.centerWrapper);
+    },
+    methods: {
+        centerWrapper() {
+            let cardWidth = 200;
+            let gapWidth = 13;
+            let paddingContainer = 30;
+            let gap = (this.$refs.wrapper.offsetWidth - 2 * paddingContainer) % (cardWidth + gapWidth);
+            if (gap < cardWidth) {
+                this.$data.centeringOffset = gap / 2;
+            } else {
+                this.$data.centeringOffset = (gap - cardWidth) / 2;
+            }
         }
     }
 }
@@ -29,13 +41,13 @@ export default {
 .wrapper {
     display: flex;
     flex-direction: row;
-    padding-inline: 30px;
+    padding-right: 30px;
+    padding-left: calc(30px + calc(v-bind(centeringOffset) * 1px));
     gap: 13px;
     flex-wrap: wrap;
     overflow: scroll;
     position: absolute;
     top: 0;
     bottom: 0;
-    left: calc(v-bind(centeringOffset) * 1px);
 }
 </style>
